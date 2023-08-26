64bff002494359b25f515d0059ea878cb75f8b58
IvanGutowskiSmith <81445359+IvanGutowskiSmith@users.noreply.github.com>
2023-08-26 15:20:30 +0100

    Upload current work

---
+++ your-script.js
@@ -0,0 +1,33 @@
+// your-script.js
+document.addEventListener("DOMContentLoaded", function () {
+  // Code to interact with the DOM once the page loads
+  const T111 = document.getElementById("111");
+  const T121 = document.getElementById("121");
+  const T131 = document.getElementById("131");
+
+  const T142 = document.getElementById("142");
+  const T152 = document.getElementById("152");
+  const T162 = document.getElementById("162");
+
+
+  button.addEventListener("click", function () {
+    const textInInt = parseInt(textIn.value);
+    if (textInInt < 5) {
+      output.textContent = "Too low"
+      
+    } else if (textInInt > 5){
+      output.textContent = "Too high"
+    }
+    else{
+      output.textContent = "Correct !"
+    }
+    });
+
+
+
+
+    class readValueIfPresent{
+
+
+    }
+});
